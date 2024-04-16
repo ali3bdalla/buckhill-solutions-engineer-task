393d19ccfa3c7857f7d55ea1ea30f5f5d5dc185d
ali abdalla <ali.dev.sd@gmail.com>
2024-04-16 17:48:23 +0200

    add existing category migration

---
+++ 01-Task/index.js
@@ -0,0 +1,13 @@
+const knex = require('knex')({
+  client: process.env.DB_DRIVER || 'mysql',
+  connection: {
+    host: process.env.DB_HOST || '', 
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "",
+  },
+});
+
+
+console.log('Creating table categories');
---
+++ 01-Task/knexfile.js
@@ -0,0 +1,63 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+
+  development: {
+    client: process.env.DB_DRIVER || 'mysql',
+    connection: {
+      database: process.env.DB_NAME || '',
+      user:     process.env.DB_USER || '',
+      password: process.env.DB_PASSWORD || '',
+      host: process.env.DB_HOST || '',
+      port: process.env.DB_PORT || 3306,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+  client: process.env.DB_DRIVER || 'mysql',
+    connection: {
+      database: process.env.DB_NAME || '',
+      user:     process.env.DB_USER || '',
+      password: process.env.DB_PASSWORD || '',
+      host: process.env.DB_HOST || '',
+      port: process.env.DB_PORT || 3306,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+client: process.env.DB_DRIVER || 'mysql',
+    connection: {
+      database: process.env.DB_NAME || '',
+      user:     process.env.DB_USER || '',
+      password: process.env.DB_PASSWORD || '',
+      host: process.env.DB_HOST || '',
+      port: process.env.DB_PORT || 3306,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
